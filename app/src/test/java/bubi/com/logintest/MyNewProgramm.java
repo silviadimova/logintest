be6597e656d4d44e58ec746ca40800fbb4ed5158
silviadimova <silvia.dimova@yahoo.com>
2019-11-26 10:40:43 +0000

    adding new method

--- app/src/test/java/bubi/com/logintest/MyNewProgramm.java
+++ app/src/test/java/bubi/com/logintest/MyNewProgramm.java
@@ -70,4 +70,20 @@ public class MyNewProgramm {
 
 
     }
+    public static void printSign (int number){
+        if (number > 0) {
+            System.out.println("Positive");
+        }
+        else if (number < 0 ){
+            System.out.println("Negative");
+        }
+        else {
+            System.out.println("Zero");
+        }
+
+
+    }
+
+
 }
+
